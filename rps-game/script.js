ebe801c862156896be24273716c261cf9dc8ce20
yogiealfin <yogiealfin23@gmail.com>
2024-01-10 07:17:04 +0700

    first step

--- rps-game/script.js
+++ rps-game/script.js
@@ -1,13 +1,46 @@
-function getComputerChoice() {
-  rng = Math.random();
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+  let result = "";
+
+  if (playerMove === "fire") {
+    if (computerMove === "fire") {
+      result = "Tie.";
+    } else if (computerMove === "water") {
+      result = "You Lose.";
+    } else {
+      result = "You Win.";
+    }
+  } else if (playerMove === "water") {
+    if (computerMove === "fire") {
+      result = "You Win.";
+    } else if (computerMove === "water") {
+      result = "Tie.";
+    } else {
+      result = "You Lose.";
+    }
+  } else {
+    if (computerMove === "fire") {
+      result = "You Lose.";
+    } else if (computerMove === "water") {
+      result = "You Win.";
+    } else {
+      result = "Tie.";
+    }
+  }
+
+  alert(`You picked ${playerMove}. Enemy picked ${computerMove}. ${result}`);
+}
+
+function pickComputerMove() {
+  const rng = Math.random();
   let computerMove;
 
   if (rng >= 0 && rng < 1 / 3) {
-    computerMove = "rock";
+    computerMove = "fire";
   } else if (rng >= 1 / 3 && rng < 2 / 3) {
-    computerMove = "paper";
+    computerMove = "water";
   } else {
-    computerMove = "scissor";
+    computerMove = "wood";
   }
   return computerMove;
 }
